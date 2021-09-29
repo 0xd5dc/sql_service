7861c2c3821a396957970c8fbac5c83e66db579d
Ergo <summermick@gmail.com>
2021-09-28 21:17:12 -0700

    dry code according to SQLalchemy documentation returning feature not supported using sqlite

--- repo.py
+++ repo.py
@@ -1,32 +1,57 @@
 """
 Attempt use repository pattern to handle data transaction but this project doesn't use external APIs and SQLalchemy already functions like a repository with SQL database
 """
+import logging
+
+from sqlalchemy import update, select, delete
 from sqlalchemy.exc import MultipleResultsFound, IntegrityError
 
-from db import User, dal, Event
+from db import User, Event
 
 
 def create_user(session, user):
-    try:
-        session.add(user)
-        session.commit()
-    except IntegrityError as error:
-        print("ignore for now")
-    return user.user_id
+    """
+    create a user
+    :param session:
+    :param user:
+    :return:
+    """
+    with session:
+        session.begin()
+        try:
+            session.add(user)
+        except Exception as e:
+            logging.error(e)
+            session.rollback()
+            raise
+        else:
+            session.commit()
 
 
 def create_event(session, event):
-    try:
-        session.add(event)
-        session.commit()
-    except IntegrityError as error:
-        print("ignore for now")
-    return event.event_id
+    """
+    create an event
+    :param session:
+    :param event:
+    :return:
+    """
+    with session:
+        session.begin()
+        try:
+            session.add(event)
+        except Exception as e:
+            logging.error(e)
+            session.rollback()
+            raise
+        else:
+            session.commit()
 
 
 def read_users(session):
     """
     read_users gets all users in the database
+    :param session:
+    :return:
     """
     return session.query(User).all()
 
@@ -34,66 +59,59 @@ def read_users(session):
 def read_events(session):
     """
     read_events gets all events in the database
+    :param session:
+    :return:
     """
     return session.query(Event).all()
 
 
 def update_user_name_by_email(session, email, name):
-    to_update = None
-    try:
-        query = session.query(User)
-        query = query.filter(User.email == email)
-        query.update({User.name: name})
+    """
+    update user name by its email
+    :param session:
+    :param email:
+    :param name:
+    :return:
+    """
+    with session:
+        user = session.execute(select(User).filter_by(email=email)).scalar_one()
+        user.name = name
         session.commit()
-        to_update = query.first()
-    except MultipleResultsFound as error:
-        print('We found too many users... is that even possible?')
-    return to_update
 
 
 def update_event_detail_by_name(session, name, detail):
-    to_update = None
-    try:
-        query = session.query(Event)
-        query = query.filter(Event.name == name)
-        query.update({Event.detail: detail})
+    """
+    update event detail by event name
+    :param session:
+    :param name:
+    :param detail:
+    :return:
+    """
+    with session:
+        user = session.execute(select(User).filter_by(name=name)).scalar_one()
+        user.detail = detail
         session.commit()
-        to_update = query.first()
-    except MultipleResultsFound as error:
-        print('We found too many users... is that even possible?')
-    return to_update
 
 
 def delete_user_by_email(session, email):
-    to_delete = None
-    try:
-        query = session.query(User)
-        query = query.filter(User.email == email)
-        to_delete = query.one()
-        session.delete(to_delete)
+    """
+    delete user by email
+    :param session:
+    :param email:
+    :return:
+    """
+    with session:
+        session.execute(delete(User).where(User.email == email))
         session.commit()
-        to_delete = query.first()
-    except MultipleResultsFound as error:
-        print('We found too many users... is that even possible?')
-    return to_delete
 
 
 def delete_event_by_name(session, name):
-    to_delete = None
-    try:
-        query = session.query(Event)
-        query = query.filter(Event.name == name)
-        to_delete = query.one()
-        session.delete(to_delete)
+    """
+    delete event by name
+    :param session:
+    :param name:
+    :return:
+    """
+    with session:
+        session.execute(delete(Event).where(Event.name == name))
         session.commit()
-        to_delete = query.first()
-    except MultipleResultsFound as error:
-        print('We found too many users... is that even possible?')
-    return to_delete
-
-
-if __name__ == '__main__':
-    create_event()
-    read_events()
-    delete_event_by_name()
-    update_event_detail_by_name()
