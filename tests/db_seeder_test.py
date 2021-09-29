ea568a247c5c8d36c36ce3845c255f12ad95e732
Ergo <summermick@gmail.com>
2021-09-28 17:18:36 -0700

    add more comments for tests

--- tests/db_seeder_test.py
+++ tests/db_seeder_test.py
@@ -6,12 +6,15 @@ from db_seeder import user_seeder, event_seeder, guest_seeder
 class MyTestCase(unittest.TestCase):
     def test_user_seeder(self):
         self.assertEqual(len(user_seeder(10)), 10)
+        # check if seeder created expected objects
 
     def test_user_seeder(self):
         self.assertEqual(len(event_seeder(3, 30)), 3)
+        # check if seeder created expected objects
 
     def test_user_seeder(self):
         self.assertEqual(len(guest_seeder(5, 10, 5)), 5)
+        # check if seeder created expected objects
 
 
 if __name__ == '__main__':
--- tests/db_test.py
+++ tests/db_test.py
@@ -9,10 +9,21 @@ class MyTestCase(unittest.TestCase):
         dal.conn_string = 'sqlite:///:memory:'
         dal.connect()
 
+    def setUp(self):
+        dal.session = dal.Session()
+
+    def tearDown(self):
+        dal.session.rollback()
+        dal.session.close()
+
     def test_tables(self):
-        session = dal.Session()
-        results = session.execute("SELECT name FROM sqlite_master").fetchall()
-        self.assertGreaterEqual(len(results), 3)  # db.py created 3 tables and the results should be at least 3
+        results = dal.session.execute("SELECT name FROM sqlite_master").fetchall()
+        self.assertGreaterEqual(len(results), 3)
+        # db.py created 3 tables and the results should be at least 3
+        self.assertIn(('users',), results)
+        self.assertIn(('events',), results)
+        self.assertIn(('guests',), results)
+        # check if table name exists
 
 
 if __name__ == '__main__':
